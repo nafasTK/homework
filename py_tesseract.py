3353b75382ae0564c63f2f51619376a90b192fce
nafasTK <148532651+nafasTK@users.noreply.github.com>
2024-09-18 00:53:05 -0700

    Add files via upload

---
+++ py_tesseract.py
@@ -0,0 +1,85 @@
+#########  importing  #########
+
+import pytesseract as pyt
+from PIL import Image
+import cv2
+
+#########  Boolean initial value  #########
+
+a = True
+b = True
+
+#########  File  #########
+
+while a == True :
+
+    ## File information ##
+    file_name = input("Please enter file name : ")
+    extension = input("Please select a file extension (png or jpg) : ")
+
+    ## Check the file extension ##
+    if extension == "png" :
+
+        ## Open the file ##
+        image = cv2.imread(f"{file_name}.png")
+        pyt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        text = pyt.image_to_string(gray_image, lang='eng')
+
+        ## Result ##
+        print(f"result :\n{text}")
+
+        ## Do it again ##
+        b = True
+        while b == True :
+            again = input("do you want to do it again ? (y/n) ")
+            if again == "y" :
+                print ("OK")
+                a = True
+                b = False
+            elif again == "n" :
+                print ("OK")
+                a = False
+                b = False
+            else :
+                print ("I didn't understand what you mean\nTry again")
+                b = True
+
+    ## Check the file extension ##
+    elif extension == "jpg" :
+
+        ## Open the file ##
+        image = cv2.imread(f"{file_name}.jpg")
+        pyt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        text = pyt.image_to_string(gray_image, lang='eng')
+
+        ## Result ##
+        print(f"result :\n{text}")
+
+        ## Do it again ##
+        b = True
+        while b == True :
+            again = input("do you want to do it again ? (y/n) ")
+            if again == "y" :
+                print ("OK")
+                a = True
+                b = False
+            elif again == "n" :
+                print ("OK")
+                a = False
+                b = False
+            else :
+                print ("I didn't understand what you mean\nTry again")
+                b = True
+
+    ## Check the file extension ##
+    else :
+        print ("It's not defined !\nChoose one of the two extensions jpg or png")
+        a = True
+        
+
+
+
+
+    
