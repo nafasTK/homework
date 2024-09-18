90bf4bcd10fcd4d2a4be1729fc9889a17fe93e08
nafasTK <148532651+nafasTK@users.noreply.github.com>
2024-09-18 01:33:03 -0700

    pytesseract.py

--- py_tesseract.py
+++ py_tesseract.py
@@ -12,7 +12,7 @@ b = True
 #########  File  #########
 
 while a == True :
-
+    b = True
     ## File information ##
     file_name = input("Please enter file name : ")
     extension = input("Please select a file extension (png or jpg) : ")
@@ -21,16 +21,28 @@ while a == True :
     if extension == "png" :
 
         ## Open the file ##
-        image = cv2.imread(f"{file_name}.png")
-        pyt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        text = pyt.image_to_string(gray_image, lang='eng')
+        try :
+            image = cv2.imread(f"{file_name}.png")
+            pyt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            text = pyt.image_to_string(gray_image, lang='eng')
+            ## Result ##
+            print(f"\nresult :\n{text}")
+
+        except FileNotFoundError:
+            print("File not found !\nTry again")
+            a = True
+            b = False
+        except :
+            print ("File not found !\nTry again")
+            a = True
+            b = False
+        
 
         ## Result ##
-        print(f"result :\n{text}")
+        # print(f"result :\n{text}")
 
         ## Do it again ##
-        b = True
         while b == True :
             again = input("do you want to do it again ? (y/n) ")
             if again == "y" :
@@ -49,16 +61,25 @@ while a == True :
     elif extension == "jpg" :
 
         ## Open the file ##
-        image = cv2.imread(f"{file_name}.jpg")
-        pyt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        text = pyt.image_to_string(gray_image, lang='eng')
+        try :
+            image = cv2.imread(f"{file_name}.png")
 
-        ## Result ##
-        print(f"result :\n{text}")
+            pyt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            text = pyt.image_to_string(gray_image, lang='eng')
+            ## Result ##
+            print(f"\nresult :\n{text}")
+
+        except FileNotFoundError:
+            print("File not found !\nTry again")
+            a = True
+            b = False
+        except :
+            print ("it's wrong")
+            a = True
+            b = False
 
         ## Do it again ##
-        b = True
         while b == True :
             again = input("do you want to do it again ? (y/n) ")
             if again == "y" :
@@ -78,8 +99,3 @@ while a == True :
         print ("It's not defined !\nChoose one of the two extensions jpg or png")
         a = True
         
-
-
-
-
-    
